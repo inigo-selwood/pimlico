8ec2b8a66a961b07fbbc3885bcfae706af9f274c
inigo-selwood <inigo.selwood@outlook.com>
2021-01-21 00:12:06 +0000

    Added methods

--- pimlico/text_buffer.hpp
+++ pimlico/text_buffer.hpp
@@ -2,6 +2,43 @@
 
 namespace Pimlico {
 
-class TextBuffer {};
+class TextBuffer {
+
+public:
+
+    unsigned int column;
+
+    unsigned long line;
+
+    char read(const char &character) {
+        return 0;
+    }
+
+    char read() {
+        return 0;
+    }
+
+    char peek(const char &character) const {
+        return 0;
+    }
+
+    char peek() const {
+        return 0;
+    }
+
+    std::string line_text() const {
+        return "";
+    }
+
+    void skip_space() {}
+    void skip_whitespace() {}
+
+    unsigned int line_indentation(const unsigned int &line_number) const {}
+
+    bool end_reached() const {
+        return true;
+    }
+
+};
 
 };
