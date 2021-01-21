a7b4d7e22890bd3a33461c2a2542fadae259386e
inigo-selwood <inigo.selwood@outlook.com>
2021-01-21 00:11:46 +0000

    Reordered members and added constructor

--- pimlico/syntax_error.hpp
+++ pimlico/syntax_error.hpp
@@ -1,16 +1,26 @@
 #pragma once
 
+#include "text_buffer.hpp"
+
 namespace Pimlico {
 
 struct SyntaxError {
 
 public:
 
-    unsigned long line;
-    unsigned int column;
-
     std::string message;
     std::string text;
+
+    unsigned int column;
+    unsigned long line;
+
+    SyntaxError(const std::string &message, const TextBuffer &buffer) {
+        this->message = message;
+
+        text = buffer.line_text();
+        column = buffer.column;
+        line = buffer.line;
+    }
 
 };
 
