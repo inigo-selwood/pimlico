bcca7712992d46cc9c372bf1b5538537b1d485de
inigo-selwood <inigo.selwood@outlook.com>
2021-01-25 00:15:15 +0000

    Added rule test(s)

---
+++ tests/specification/test.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+#include <string>
+#include <vector>
+
+#include "text_buffer.hpp"
+#include "syntax_error.hpp"
+
+#include "pimlico.hpp"
+using namespace Pimlico;
+
+int main(int argument_count, char *argument_values[]) {
+    if(argument_count != 2)
+        return 5;
+
+    std::string grammar_filename = argument_values[1];
+    std::ifstream grammar_stream(grammar_filename);
+    if(grammar_stream.is_open() == false)
+        return 4;
+
+    std::string grammar_string((std::istreambuf_iterator<char>(grammar_stream)),
+                 std::istreambuf_iterator<char>());
+
+    std::vector<SyntaxError> errors;
+    try {
+        const auto specification = Specification::parse(grammar_string, errors);
+        if(specification == nullptr) {
+            for(const SyntaxError &error : errors)
+                std::cerr << error << "\n";
+            return 1;
+        }
+
+        std::cout << *specification << "\n";
+
+        return 0;
+    }
+    catch(ParseLogicError &exception) {
+        std::cerr << exception.what() << "\n";
+        for(const SyntaxError &error : errors)
+            std::cerr << error << "\n";
+        return 2;
+    }
+}
---
+++ tests/specification/test.sh
@@ -0,0 +1,47 @@
+g++-7 ./tests/specification/test.cpp \
+        -o ./tests/specification/test \
+        -I ./pimlico/ \
+        -std=c++17
+
+if [[ $? -ne 0 ]]; then
+    printf "compilation failed\n"
+    exit 1
+fi
+
+test_script=$1
+if [[ ! -z $test_script ]]; then
+    ./tests/specification/test $test_script 2> errors.txt 1> output.txt
+
+    return_value=$?
+    test_name="$(basename $test_script .peg)"
+
+    # Report parse failures
+    if [[ $return_value -eq 1 ]]; then
+        printf "! ${test_name} (failed)\n"
+
+        while read line; do
+            printf "    ${line}\n"
+        done < errors.txt
+
+    # Report incomplete parses
+    elif [[ $return_value -eq 2 ]]; then
+        printf "! ${test_name} (threw exception)\n"
+
+        while read line; do
+            printf "    ${line}\n"
+        done < errors.txt
+
+    # Print output
+    else
+        printf "  ${test_name} (passed)\n"
+
+        while read -r line; do
+            printf "    ${line}\n"
+        done < output.txt
+    fi
+else
+    printf "no grammar file specified\n"
+    exit 1
+fi
+
+rm -f errors.txt output.txt ./tests/specification/test
