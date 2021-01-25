1ac8f256d0bb478bd18a395e1f11c55033b38281
inigo-selwood <inigo.selwood@outlook.com>
2021-01-25 12:43:12 +0000

    Removed std::exception inheritance

--- pimlico/parse_logic_error.hpp
+++ pimlico/parse_logic_error.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <exception>
 #include <sstream>
 #include <string>
 
@@ -12,12 +11,6 @@ public:
 
     ParseLogicError(const std::string &message, const TextBuffer &buffer) :
             SyntaxError(message, buffer) {}
-
-    const char *what() const throw() {
-        std::stringstream stream;
-        stream << *this;
-        return stream.str().c_str();
-    }
 
 };
 
