a62a5195dbad4e21c841bc7467f02d7fb75002d2
inigo-selwood <inigo.selwood@outlook.com>
2021-01-31 00:11:40 +0000

    Added scope

--- pimlico/syntax_tree.hpp
+++ pimlico/syntax_tree.hpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 
-#include "syntax_error.hpp"
 #include "text_buffer.hpp"
 
 #include "specification.hpp"
@@ -20,7 +19,7 @@ public:
 
     std::unique_ptr<SyntaxTree> parse(const Specification &specification,
             const std::string &source,
-            const std::vector<SyntaxError> &errors);
+            const std::vector<TextBuffer::SyntaxError> &errors);
 
 };
 
