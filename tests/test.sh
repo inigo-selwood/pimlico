d9874e95e74e19ed4da89f2d64871b1904798f85
inigo-selwood <inigo.selwood@outlook.com>
2021-02-02 21:57:22 +0000

    Added verbose flag to holistic test

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,12 @@
 
+verbose=0
+if [[ "$*" == *--verbose* ]]; then
+    verbose_flag="--verbose"
+fi
 
 printf "testing terms\n"
-(cd ./grammar/term && bash ../test.sh)
+(cd ./grammar/term && bash ../test.sh ${verbose_flag})
 
 printf "\n"
 printf "testing specifications\n"
-(cd ./grammar/specification && bash ../test.sh)
+(cd ./grammar/specification && bash ../test.sh ${verbose_flag})
