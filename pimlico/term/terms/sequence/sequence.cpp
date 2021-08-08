15bbd7f438139fe1514afcc6a0ef9a22936171ae
inigo-selwood <inigo.selwood@outlook.com>
2021-08-08 15:46:32 +0100

    Split out parse function

--- pimlico/term/terms/sequence/sequence.cpp
+++ pimlico/term/terms/sequence/sequence.cpp
@@ -3,25 +3,3 @@
 #include "../choice/choice.hpp"
 
 Sequence::Sequence() : Term(Term::Type::SEQUENCE) {}
-
-Term *Sequence::parse(ParseBuffer &buffer, ErrorBuffer &errors) {
-    Sequence *sequence = new Sequence();
-    sequence->position = buffer.position;
-
-    while(true) {
-
-        Term *value = Choice::parse(buffer, errors);
-        if(value == nullptr)
-            return nullptr;
-        sequence->values.push_back(value);
-
-        buffer.skip(true);
-        if(buffer.finished() || buffer.peek('\n') || buffer.peek(')'))
-            break;
-    }
-
-    if(sequence->values.size() == 1)
-        return sequence->values.back();
-
-    return sequence;
-}
