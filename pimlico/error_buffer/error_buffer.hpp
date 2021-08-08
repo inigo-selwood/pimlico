82376934d2c2fb0ceb50398eb4b7d0050e32f06c
inigo-selwood <inigo.selwood@outlook.com>
2021-08-08 23:30:36 +0100

    Added print method

--- pimlico/error_buffer/error_buffer.hpp
+++ pimlico/error_buffer/error_buffer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+#include <utility>
 #include <string>
 
 #include "../text_position/text_position.hpp"
@@ -8,6 +10,15 @@ class ErrorBuffer {
 
 public:
 
+    friend std::ostream &operator<<(std::ostream &stream, 
+            const ErrorBuffer &buffer);
+
     void add(const std::string &message, const TextPosition &position);
+
+    void print(std::ostream &stream) const;
+
+private:
+
+    std::vector<std::pair<std::string, TextPosition>> errors;
 
 };
