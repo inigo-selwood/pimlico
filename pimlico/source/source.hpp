18b317044c649075132546913479c5fbfcf637e1
inigo-selwood <inigo.selwood@outlook.com>
2021-08-08 21:33:15 +0100

    Switched to map rule parsing

--- pimlico/source/source.hpp
+++ pimlico/source/source.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <map>
+
 #include "../rule/rule.hpp"
 
 class Source {
 
 public:
 
-    std::vector<Rule *> rules;
+    std::map<int, Rule *> rules;
+
+    static Source *parse(const std::string &grammar, ErrorBuffer &errors);
+
+    ~Source();
 
 };
