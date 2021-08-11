4a07b39ae2b7d91ae74fd94609635e61903440f5
inigo-selwood <inigo.selwood@outlook.com>
2021-08-11 09:34:48 +0100

    Migrated to switch

--- pimlico/parse_buffer/increment.cpp
+++ pimlico/parse_buffer/increment.cpp
@@ -8,47 +8,49 @@ Will also update values like the line and column number, the current
 indentation, and whether the line is broken
 */
 void ParseBuffer::increment() {
-    if(finished())
+    TextPosition &position = this->position;
+    if(position.index == this->length)
         return;
 
     position.index += 1;
-    char character = get_character();
 
-    TextPosition &position = this->position;
+    switch(this->text[position.index]) {
 
-    // Handle newlines
-    if(character == '\n') {
+        // Handle newlines 
+        case '\n': {
 
-        // This target, if met, means the line is unbroken
-        int indentation_target = position.block_indentation + 8;
-        int indentation = this->line_indices[position.line].second;
-        int line_count = line_indices.size();
+            // Pull out some values for ease-of-reading
+            int target = position.block_indentation + 8;
+            int line_count = this->line_indices.size();
+            int indentation = this->line_indices[position.line].second;
+            
+            // Check whether the line is broken
+            if((position.line < line_count) && (indentation >= target))
+                position.line_broken = true;
+            else {
+                position.line_broken = false;
+                if(position.line < line_count)
+                    position.block_indentation = indentation;
+            }
 
-        // Check if the line is not the last one, and if the target is met
-        position.line_broken = false;
-        if((position.line < line_count) && (indentation >= indentation_target))
-            position.line_broken = true;
-
-        // Otherwise, recalculate the current indentation
-        else if(position.line < line_count) {
-            position.block_indentation =
-                    this->line_indices[position.line].second;
+            // Increment the line, column values
+            position.line += 1;
+            position.column = 0;
+            break;
         }
-
-        // Increment the line, column values
-        position.line += 1;
-        position.column = 1;
+        
+        // Handle tabs 
+        case '\t':
+            position.column += (5 - position.column % 5);
+            break;
+        
+        // Otherwise, (assuming the character is in the range [ -~], increment 
+        // the column
+        default:
+            if(this->text[position.index] < ' ' 
+                    || this->text[position.index] > '~')
+                position.column += 1;
     }
-
-    // Handle horizontal tabs
-    else if(this->text[position.index] == '\t') {
-        int remainder = position.column % 5;
-        position.column += (5 - remainder);
-    }
-
-    // Handle carriage-returns
-    else if(this->text[position.index] != '\r')
-        position.column += 1;
 }
 
 /* Increment the buffer's position
