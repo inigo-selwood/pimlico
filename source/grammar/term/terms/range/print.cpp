27271250f87b6c3dc90c60546893a0e5043a497c
inigo-selwood <inigo.selwood@outlook.com>
2021-12-27 21:31:28 +0000

    Silenced escape codes (for now)

--- source/grammar/term/terms/range/print.cpp
+++ source/grammar/term/terms/range/print.cpp
@@ -10,32 +10,35 @@ stream
     the stream to print to
 */
 void Range::print(std::ostream &stream) const {
-    stream << "[\'";
-
-    static const std::map<char, std::string> escape_codes = {
-        {'\n', "\\n"},
-        {'\r', "\\r"} ,
-        {'\b', "\\b"},
-        {'\t', "\\t"},
-        {'\\', "\\\\"},
-        {'\"', "\\\""},
-        {'\'', "\\\'"},
-    };
+    // stream << "[\'";
+    //
+    // static const std::map<char, std::string> escape_codes = {
+    //     {'\n', "\\n"},
+    //     {'\r', "\\r"} ,
+    //     {'\b', "\\b"},
+    //     {'\t', "\\t"},
+    //     {'\\', "\\\\"},
+    //     {'\"', "\\\""},
+    //     {'\'', "\\\'"},
+    // };
+    //
+    // const std::array<char, 2> &range = this->value;
+    // if(escape_codes.find(range[0]) != escape_codes.end())
+    //     stream << escape_codes.at(range[0]);
+    // else
+    //     stream << range[0];
+    //
+    // stream << "' - '";
+    //
+    // if(escape_codes.find(range[1]) != escape_codes.end())
+    //     stream << escape_codes.at(range[1]);
+    // else
+    //     stream << range[1];
+    //
+    // stream << "\']";
 
     const std::array<char, 2> &range = this->value;
-    if(escape_codes.find(range[0]) != escape_codes.end())
-        stream << escape_codes.at(range[0]);
-    else
-        stream << range[0];
-
-    stream << "' - '";
-
-    if(escape_codes.find(range[1]) != escape_codes.end())
-        stream << escape_codes.at(range[1]);
-    else
-        stream << range[1];
-
-    stream << "\']";
+    stream << "['" << range[0] << "' - '" << range[1] << "']";
 }
 
 }; // Namespace Pimlico
