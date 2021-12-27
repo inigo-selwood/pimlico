133edcbe3a6e2d772c4095c99c6d4c8257cdf7b9
inigo-selwood <inigo.selwood@outlook.com>
2021-12-27 21:01:54 +0000

    Moved to post-increment line/column updating

--- source/buffer/parse/increment.cpp
+++ source/buffer/parse/increment.cpp
@@ -11,37 +11,31 @@ indentation, and whether the line is broken
 */
 void Parse::increment() {
     Buffer::Position &position = this->position;
-    if(position.index == this->length)
+    if(position.index >= this->length)
         return;
 
+    switch(this->text[position.index]) {
+
+        // Handle newlines, increment the line, column values
+        case '\n':
+            position.line += 1;
+            position.column = 1;
+            break;
+
+        // Handle tabs, increase indentation up to the next multiple of 4
+        case '\t':
+            position.column = ((position.column + 3) / 4) * 4 + 1;
+            break;
+
+        // Otherwise, (assuming the character is in the range [ -~],
+        // increment the column
+        default:
+            if(this->text[position.index] >= ' '
+                    && this->text[position.index] <= '~')
+                position.column += 1;
+    }
+
     position.index += 1;
-
-    if(position.index < this->length) {
-        switch(this->text[position.index]) {
-
-            // Handle newlines, increment the line, column values
-            case '\n':
-                position.line += 1;
-                position.column = 1;
-                break;
-
-            // Handle tabs, increase indentation up to the next multiple of 4
-            case '\t':
-                position.column = ((3 - position.column) & ~0x03) + 1;
-                break;
-
-            // Otherwise, (assuming the character is in the range [ -~],
-            // increment the column
-            default:
-                if(this->text[position.index] >= ' '
-                        && this->text[position.index] <= '~')
-                    position.column += 1;
-        }
-    }
-    else {
-        position.column = 0;
-        position.line = 0;
-    }
 }
 
 /* Increment the buffer's position
