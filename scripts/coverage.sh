98a60d4a180fe26448b1ce536a122b3644983c6d
inigo-selwood <inigo.selwood@outlook.com>
2022-03-27 12:27:05 +0100

    Fixed coverage scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,14 +1,25 @@
 cmake .
+if [[ $? -ne 0 ]]; then
+    echo "cmake failed"
+    exit 1
+fi
 
-make test -j12
+make test --jobs=12
+if [[ $? -ne 0 ]]; then
+    echo "build failed"
+    exit 1
+fi
 
-mkdir -p coverage
+mkdir --parents coverage
 
 ./test/binaries/test
 
+ROOT=`pwd`
+
 gcovr \
-    --root source/ \
-    --object-directory CMakeFiles/ \
+    --root $ROOT \
+    --filter $ROOT/source/ \
+    --object-directory $ROOT/CMakeFiles/ \
     --delete \
-    --html-details coverage/ \
-    -j12
+    --output coverage/ \
+    --html-details
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,15 @@
 cmake .
+if [[ $? -ne 0 ]]; then
+    echo "cmake failed"
+    exit 1
+fi
 
 make test -j12
+if [[ $? -ne 0 ]]; then
+    echo "build failed"
+    exit 1
+fi
 
-mkdir -p coverage
+mkdir --parents coverage
 
 ./test/binaries/test
