91533ce5fbae8f9b17e5d36fce7bf02466b0b056
inigo-selwood <inigo.selwood@outlook.com>
2022-04-23 17:51:23 +0100

    Fresh start

--- source/grammar/term/terms/constant/constant.cpp
+++
@@ -1,8 +0,0 @@
-#include "constant.hpp"
-
-namespace Pimlico {
-
-// Constructor
-Constant::Constant() : Term(Term::Type::CONSTANT) {}
-
-}; // Namespace Pimlico
--- source/parse_buffer/error/instance/message/print.cpp
+++
@@ -1,9 +0,0 @@
-#include "message.hpp"
-
-namespace Pimlico {
-
-void Message::print(std::ostream &stream) const {
-    stream << "(" << this->source << ") " << this->text;
-}
-
-}; // Namespace Pimlico
