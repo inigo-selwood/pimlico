f595468721ab67c1ba7376d2b0fb4db53e3c4808
inigo-selwood <inigo.selwood@outlook.com>
2022-05-23 15:56:47 +0100

    Fixed excerpt pointer offset bug

--- source/text/excerpt.py
+++ source/text/excerpt.py
@@ -12,17 +12,18 @@ class Excerpt:
             position: text.Position = None):
         
         self.position = copy(position) if position else buffer.position
-        self.text = buffer.line_text(self.position.line)
+        self.text = buffer.line_text(self.position.line).strip()
+
+        self.pointer_index = 0
+        if position.column > 0:
+            indentation = buffer.line_indentation(position.line)
+            self.pointer_index = position.column - indentation - 1
+        if position.column < 0:
+            self.pointer_index = len(self.text.rstrip())
     
     def __str__(self):
         stripped_text = self.text.lstrip()
-        indentation = len(self.text) - len(stripped_text)
-
-        column = self.position.column
-        if column == -1:
-            column = len(stripped_text) + 1
-        
-        offset = ' ' * (column - indentation - 1)
+        offset = ' ' * self.pointer_index
         
         return (f'{stripped_text}'
                 f'\n{offset}^')
