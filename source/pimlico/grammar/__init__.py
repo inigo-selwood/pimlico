54df845ae90da5b0a2aefcdd2f181d04a48bc594
inigo-selwood <inigo.selwood@outlook.com>
2022-09-24 07:45:26 +0100

    clean slate

--- source/pimlico/grammar/__init__.py
+++
@@ -1,8 +0,0 @@
-from .expression import Expression
-from .rule import Rule
-from .source import Source
-from .term import Term
-
-from .terms import *
-
-__all__ = ["Expression", "Rule", "Source", "Term"]
--- source/pimlico/grammar/terms/__init__.py
+++
@@ -1,9 +0,0 @@
-from .choice import Choice
-from .constant import Constant
-from .range import Range
-from .reference import Reference
-from .sequence import Sequence
-from .set import Set
-
-
-__all__ = ["Choice", "Constant", "Range", "Reference", "Sequence", "Set"]
